68dc543aa56f64c3ae0ad4e33c35f9791faf54df
nozhora <nozhora510@gmail.com>
2025-11-15 12:32:07 +0300

    jdfkl

--- tests/integration.test.js
+++ tests/integration.test.js
@@ -51,7 +51,7 @@ describe("Integration Tests", () => {
       };
 
       const response = await request(app)
-        .post("/api/auth/")
+        .post("/api/auth/register")
         .send(newUserData)
         .expect(200);
 
